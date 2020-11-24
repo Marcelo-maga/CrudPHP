daa94a26d3a9d1f01be980dd089c42abc785b0b1
Marcelo Magalhães <marcelo.magalhaes13@etec.sp.gov.br>
2020-11-24 11:56:54 -0300

    Upload de arquivos PNG

---
+++ upar.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    <head>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="arquivo" id="arquivo"/></br>
+            <input type="submit" value="salvar" />
+            <input type="reset" value="Novo" />
+        </form>
+    </body>
+</html>
